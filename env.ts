be7720e51967b945c608d946efff747f0ca55d5b
Yuxuan Xie <ykuyo.dev@gmail.com>
2025-02-20 15:34:33 -0800

    Moved env vars.

--- env.ts
+++ env.ts
@@ -1,13 +1,13 @@
 import { CLUSTER_ENV_VARS, ClusterEnvVars } from "@phading/cluster/env";
 
 export interface EnvVars extends ClusterEnvVars {
-  releaseServiceName?: string;
   databaseId?: string;
   databaseInstanceId?: string;
   sessionSecretKeyFile?: string;
+  releaseServiceName?: string;
+  port?: number;
   builderAccount?: string;
   serviceAccount?: string;
-  port?: number;
 }
 
 export let ENV_VARS: EnvVars = CLUSTER_ENV_VARS;
