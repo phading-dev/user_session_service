b1f0cedcd2a440acaffb24bca3dcabb958d5a702
Ykuyo <ykuyo.dev@gmail.com>
2025-06-13 19:33:59 -0700

    Added resources.

--- dev/env.ts
+++ dev/env.ts
@@ -4,3 +4,5 @@ import { ENV_VARS } from "../env_vars";
 
 ENV_VARS.spannerInstanceId = ENV_VARS.balancedSpannerInstanceId;
 ENV_VARS.replicas = 1;
+ENV_VARS.cpu = "200m";
+ENV_VARS.memory = "256Mi";
--- release_template.ts
+++ release_template.ts
@@ -113,6 +113,13 @@ spec:
             port: ${ENV_VARS.port}
           initialDelaySeconds: 10
           periodSeconds: 10
+        resources:
+          requests:
+            cpu: "${ENV_VARS.cpu}"
+            memory: "${ENV_VARS.memory}"
+          limits:
+            cpu: "${ENV_VARS.cpu}"
+            memory: "${ENV_VARS.memory}"
 ---
 apiVersion: monitoring.googleapis.com/v1
 kind: PodMonitoring
