dbe866c9ee2c3cae154e34011a0e99ba7e675457
Ykuyo <ykuyo.dev@gmail.com>
2025-07-26 12:29:00 -0700

    Adjusted resources.

--- prod/env.ts
+++ prod/env.ts
@@ -4,6 +4,6 @@ import { ENV_VARS } from "../env_vars";
 
 ENV_VARS.spannerInstanceId = ENV_VARS.balancedSpannerInstanceId;
 ENV_VARS.bigtableInstanceId = ENV_VARS.singleBigtableInstanceId;
-ENV_VARS.replicas = 1;
-ENV_VARS.cpu = "400m";
+ENV_VARS.replicas = 2;
+ENV_VARS.cpu = "300m";
 ENV_VARS.memory = "512Mi";
